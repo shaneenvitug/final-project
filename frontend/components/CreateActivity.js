92d5111b005290993033fbe1639cd06254ed3399
Shaneen Vitug <shaneenvitug@gmail.com>
2019-12-16 18:38:40 +1100

    uploading images via cloudinary

--- frontend/components/CreateActivity.js
+++ frontend/components/CreateActivity.js
@@ -41,6 +41,24 @@ class CreateActivity extends Component {
     this.setState({ [name]: val });
   }
 
+  uploadFile = async e => {
+    console.log('uploading');
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'jeepney');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/didhtdmyv/image/upload', {
+      method: 'POST',
+      body: data
+    });
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  }
+
   render() {
     return (
       <Mutation mutation={CREATE_ACTIVITY_MUTATION} variables={this.state}>
@@ -55,6 +73,12 @@ class CreateActivity extends Component {
           }}>
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input type="file" id="file" name="file" placeholder="Upload an image" required onChange={this.uploadFile}/>
+                {this.state.image && <img width="200" src={this.state.image} alt="Upload Preview" />}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title} onChange={this.handleChange}/>
